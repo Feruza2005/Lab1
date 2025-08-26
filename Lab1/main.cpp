5ffa37407f665733c7b026e7df88a6ab8efc695a
Feruza Jo'rayeva <f.jorayeva@newuu.uz>
2025-08-26 11:27:10 +0500

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Feruza Jorayeva on 26/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+    
+    
+}
